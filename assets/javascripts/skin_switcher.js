2944a7c4933e06ca7ce7826a7515ab4f683122ff
Thea Choem <theacheng.g6@gmail.com>
2023-02-05 14:55:53 +0700

    feat: toggle & switch theme

---
+++ assets/javascripts/skin_switcher.js
@@ -0,0 +1,91 @@
+const SkinSwitcher = {
+  selectorId: "theme-mode-switch-container",
+  init: function () {
+    this.listenToSystemTheme();
+    this.initializeTheme();
+  },
+  listenToSystemTheme: function () {
+    window
+      .matchMedia("(prefers-color-scheme: dark)")
+      .addEventListener("change", (event) => {
+        const isDarkMode = event.matches;
+        this.changeSkin(isDarkMode, solarized === "true");
+      });
+  },
+  initializeTheme: function () {
+    darkMode = localStorage.getItem("storage-dark-mode");
+    solarized = localStorage.getItem("storage-solarized");
+
+    if (darkMode && solarized) {
+      this.changeSkin(darkMode === "true", solarized === "true");
+    } else {
+      if (window.matchMedia) {
+        if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+          this.changeSkin(true, true);
+        } else {
+          this.changeSkin(false, true);
+        }
+      }
+    }
+
+    document.body.classList.remove("d-none");
+  },
+  replaceStyleSheetWith: function (skin) {
+    var metaTag = document.getElementById("stylesheet");
+    var SSHref = metaTag.href;
+    var SSName = SSHref.substring(SSHref.lastIndexOf("/") + 1);
+
+    metaTag.href = SSHref.replace(SSName, skin);
+  },
+  setThemeSwitcherContent: function (isDarkMode, solarized) {
+    element = document.getElementById(this.selectorId);
+    element.setAttribute("data-dark-mode", isDarkMode);
+    element.setAttribute("data-solarized", solarized);
+
+    if (isDarkMode) {
+      element.innerHTML = solarized ? "nights_stay" : "dark_mode";
+    } else {
+      element.innerHTML = solarized ? "wb_incandescent" : "light_mode";
+    }
+  },
+  constructSkin: function (isDarkMode, solarized) {
+    if (isDarkMode) {
+      return solarized ? "solarized-dark.css" : "dark.css";
+    } else {
+      return solarized ? "solarized-light.css" : "classic.css";
+    }
+  },
+  changeSkin: function (isDarkMode, solarized) {
+    this.setThemeSwitcherContent(isDarkMode, solarized);
+    this.replaceStyleSheetWith(this.constructSkin(isDarkMode, solarized));
+
+    localStorage.setItem("storage-dark-mode", isDarkMode);
+    localStorage.setItem("storage-solarized", solarized);
+  },
+  isSolarized: function () {
+    solarized = document
+      .getElementById(this.selectorId)
+      .getAttribute("data-solarized");
+    return solarized === "true";
+  },
+  isDarkMode: function () {
+    darkMode = document
+      .getElementById(this.selectorId)
+      .getAttribute("data-dark-mode");
+    return darkMode === "true";
+  },
+  next: function () {
+    var darkMode = this.isDarkMode();
+    var solarize = this.isSolarized();
+
+    var lightMode = !darkMode;
+    var notSolarize = !solarize;
+
+    if (lightMode && notSolarize) this.changeSkin(true, false);
+    if (darkMode && notSolarize) this.changeSkin(false, true);
+    if (lightMode && solarize) this.changeSkin(true, true);
+    if (darkMode && solarize) this.changeSkin(false, false);
+  },
+};
+
+SkinSwitcher.init();
--- index.md
+++ index.md
@@ -4,28 +4,3 @@
 
 layout: home
 ---
-
-<div id="skin-switch-container">
-  <select id="skin-switch" onchange="changeSkin()">
-    {%- for skin in page.available_skins %}
-    {% if skin == 'classic' -%}
-    <option value="style.css">classic</option>
-    {%- else -%}
-    <option value="{{ skin }}.css">{{ skin }}</option>
-    {%- endif %}
-    {%- endfor %}
-  </select>
-</div>
-<br>
-
-<script>
-  document.getElementById("skin-switch-container").style.display = "block";
-  var metaTag = document.getElementById("stylesheet");
-  var SSHref = metaTag.href;
-  var SSName = (SSHref.substring(SSHref.lastIndexOf('/') + 1));
-
-  document.getElementById("skin-switch").value = SSName;
-  function changeSkin() {
-    metaTag.href = SSHref.replace(SSName, event.target.value);
-  }
-</script>
